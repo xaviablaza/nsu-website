1f45c57c7662147018f11adee98f1fe915766069
Michael Pollind <polli104@mail.chapman.edu>
2017-09-09 08:34:44 -0700

    tweaked font sizes

--- themes/source-flask-theme/src/js/script.js
+++ themes/source-flask-theme/src/js/script.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap'
 
 $(document).ready(() => {
     let timeout = null;
-    let target = $("#title-card").offset().top +  $("#title-card").height()
+    let target = $("#title-card").offset().top + $("#title-card").height()
 
     $(window).scroll(function () {
         if (!timeout) {
@@ -13,55 +13,55 @@ $(document).ready(() => {
                 clearTimeout(timeout)
                 timeout = null;
                 if ($(window).scrollTop() >= target) {
-                  $('#header').removeClass('nav-hidden')
+                    $('#header').removeClass('nav-hidden')
                 } else {
-                  $('#header').addClass('nav-hidden')
+                    $('#header').addClass('nav-hidden')
                 }
             }, 250)
         }
     })
 
-  // Set the date we're counting down to
-  $('.vote-btn').hide()
-  var voteStart = new Date('Sep 18, 2017 00:00:00').getTime()
-  var voteEnd = new Date('Sep 20, 2017 17:00:00').getTime()
-  // Update the count down every 1 second
-  var x = setInterval(() => {
-    // Get todays date and time
-    var now = new Date().getTime()
+    // Set the date we're counting down to
+    $('.vote-btn').hide()
+    var voteStart = new Date('Sep 18, 2017 00:00:00').getTime()
+    var voteEnd = new Date('Sep 20, 2017 17:00:00').getTime()
+    // Update the count down every 1 second
+    var x = setInterval(() => {
+        // Get todays date and time
+        var now = new Date().getTime()
 
-    // Find the distance between now an the count down date
-    var distance = -1
+        // Find the distance between now an the count down date
+        var distance = -1
 
-    if (voteStart - now > 0) {
-      distance = voteStart - now
-      document.getElementById('vote-title').innerHTML = 'voting starts in'
-      document.getElementById('voting-flag').innerHTML = 'voting starts in'
-    } else if (voteEnd - now > 0) {
-      distance = voteEnd - now
-      document.getElementById('vote-title').innerHTML = 'voting ends in'
-      document.getElementById('voting-flag').innerHTML = 'voting ends in'
-      $('.vote-btn').show()
-    } else {
-      document.getElementById('vote-title').innerHTML = 'voting has ended!'
-      document.getElementById('voting-flag').innerHTML = 'voting has ended!'
-    }
+        if (voteStart - now > 0) {
+            distance = voteStart - now
+            document.getElementById('vote-title').innerHTML = 'voting starts in'
+            document.getElementById('voting-flag').innerHTML = 'voting starts in'
+        } else if (voteEnd - now > 0) {
+            distance = voteEnd - now
+            document.getElementById('vote-title').innerHTML = 'voting ends in'
+            document.getElementById('voting-flag').innerHTML = 'voting ends in'
+            $('.vote-btn').show()
+        } else {
+            document.getElementById('vote-title').innerHTML = 'voting has ended!'
+            document.getElementById('voting-flag').innerHTML = 'voting has ended!'
+        }
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-    // Display the result in the element with id="demo"
-    document.getElementById('clock').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's '
-    document.getElementById('clock1').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's '
+        // Display the result in the element with id="demo"
+        document.getElementById('clock').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's '
+        document.getElementById('clock1').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's '
 
-    // If the count down is finished, write some text 
-    if (distance < 0) {
-      clearInterval(x)
-      document.getElementById('clock').innerHTML = ''
-      $('.vote-btn').hide()
-    }
-  }, 1000)
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x)
+            document.getElementById('clock').innerHTML = ''
+            $('.vote-btn').hide()
+        }
+    }, 1000)
 })
