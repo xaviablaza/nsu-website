0ab4b59db7dcf8f6d21f0b3451275dcf79a7972b
Michael Pollind <polli104@mail.chapman.edu>
2017-09-09 00:51:14 -0700

    added nav bar

--- themes/source-flask-theme/src/js/script.js
+++ themes/source-flask-theme/src/js/script.js
@@ -1,42 +1,23 @@
-import $ from 'jquery'
-// import TweenMax from 'gsap/TweenMax'
-// import TimelineMax from 'gsap/TimelineMax'
+import 'tether'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/js/bootstrap'
 
 $(document).ready(() => {
-  /* Smooth scrolling */
-  // Select all links with hashes
-  $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-    // On-page links
-      if (
-        window.location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
-        window.location.hostname === this.hostname
-      ) {
-      // Figure out element to scroll to
-        var target = $(this.hash)
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
-        // Does a scroll target exist?
-        if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-          event.preventDefault()
-          $('html, body').animate({
-            scrollTop: target.offset().top - 70
-          }, 1000, function () {
-          // Callback after animation
-          // Must change focus!
-            var $target = $(target)
-            $target.focus()
-            if ($target.is(':focus')) { // Checking if the target was focused
-              return false
-            } else {
-              $target.attr('tabindex', '-1') // Adding tabindex for elements not focusable
-              $target.focus() // Set focus again
-            }
-          })
+    let timeout = null;
+    let target = $("#title-card").offset().top +  $("#title-card").height()
+
+    $(window).scroll(function () {
+        if (!timeout) {
+            timeout = setTimeout(function () {
+                clearTimeout(timeout)
+                timeout = null;
+                if ($(window).scrollTop() >= target) {
+                  $('#header').removeClass('nav-hidden')
+                } else {
+                  $('#header').addClass('nav-hidden')
+                }
+            }, 250)
         }
-      }
     })
 })
